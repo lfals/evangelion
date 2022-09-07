a1747e2df70b26a110a7cecfe4bbcabdad529e58
Luis Amorim <luisfelipeamorim@hotmail.com>
2022-09-07 03:00:36 -0300

    Add files via upload

---
+++ init.bat
@@ -0,0 +1,15 @@
+
+
+@REM wsl --install -d  Ubuntu
+
+pause 
+
+curl "https://code.visualstudio.com/sha/download?build=stable&os=win32-x64" 
+
+@REM ./vsc.exe
+
+pause 
+
+curl https://raw.githubusercontent.com/lfals/evangelion/master/init.sh --output init.sh
+
+pause
--- init.sh
+++ init.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo 'hi'
+# sudo apt update && sudo apt upgrade
+
+# sudo apt install curl
+
+
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
+
+sudo apt-get install nodejs
+
+read -p "Press enter to continue" "mainmenuinput"
+
+node -v
+
+npm -v 
+
+npx -v
